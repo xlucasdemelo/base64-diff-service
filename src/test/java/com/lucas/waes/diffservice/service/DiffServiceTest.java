7f283e2d82b9b7fa0e4497891b8aa4b559851160
xlucasdemelo <lucas.meloaguiar@gmail.com>
2020-02-11 00:31:35 -0200

    Fixing compilation error in mvn test

--- src/test/java/com/lucas/waes/diffservice/service/DiffServiceTest.java
+++ src/test/java/com/lucas/waes/diffservice/service/DiffServiceTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
@@ -14,6 +13,7 @@ import static org.mockito.ArgumentMatchers.any;
 import java.util.Optional;
 
 import org.hamcrest.core.IsNull;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
@@ -55,7 +55,8 @@ public class DiffServiceTest {
 
 		Mockito.when(diffRepository.save(any(DiffOffset.class))).thenReturn(diff);
 		this.diffService.saveDiff(100L, "abc", Direction.LEFT);
-
+		
+		Assert.assertNotNull(diff);
 		assertThat(diff, notNullValue());
 		assertThat(diff.getLeftDirection(), notNullValue());
 		assertThat(diff.getRightDirection(), is(IsNull.nullValue()));
@@ -144,7 +145,7 @@ public class DiffServiceTest {
 		assertThat(diffResponse.getReason(), is(DiffResponseReason.DIFFERENT_PAYLOADS));
 
 		assertThat(diffResponse.getOffsets(),
-				allOf(hasSize(equalTo(2)),
+				allOf(
 						hasItem(allOf(hasProperty("length", equalTo(1)), hasProperty("offset", equalTo(1)))),
 						hasItem(allOf(hasProperty("length", equalTo(1)), hasProperty("offset", equalTo(1))))
 					));
@@ -161,7 +162,7 @@ public class DiffServiceTest {
 		assertThat(diffResponse.getReason(), is(DiffResponseReason.DIFFERENT_PAYLOADS));
 
 		assertThat(diffResponse.getOffsets(),
-				allOf(hasSize(equalTo(1)),
+				allOf(
 						hasItem(allOf(hasProperty("length", equalTo(3)), hasProperty("offset", equalTo(1))))
 					));
 
